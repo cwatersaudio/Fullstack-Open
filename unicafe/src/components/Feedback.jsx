f4e766a47215cfeaacd135cc73bf5531a7af7fad
Chris Waters <chriswatersaudio@gmail.com>
2023-11-06 12:40:26 -0500

    trying to get event handler to work

--- unicafe/src/components/Feedback.jsx
+++ unicafe/src/components/Feedback.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
 export default function Feedback(props) {
+    const [feedback,updateFeedback] = React.useState({good: 0,neutral:0,bad:0})
+    
+
+    function butClick(event) {
+        updateFeedback(prevFeedback => {
+            
+            return {
+            ...prevFeedback,
+            [prevFeedback.event.name] : prevFeedback.event + 1 
+            }
+        })
+    }
+    
     return (
         <>
             <h2>Give Feedback:</h2>
-            <button>Good</button>
-            <button>Neutral</button>
-            <button>Bad</button>
+            <button name="good" onClick={butClick}>Good</button>
+            <button name="neutral" onClick={butClick}>Neutral</button>
+            <button name="bad" onClick={butClick}>Bad</button>
         
         </>
     )
