5125f203cf6f01056daa662a1c1c2e9017d80704
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-05 21:05:03 -0500

    first commit

---
+++ 1:Intro to React/courseinfo/courseinfo/src/components/Content.jsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+export default function Content (props) {
+    
+    return (
+        <>
+            <p> {part1} {exercises1} </p>
+            <p>{part2} {exercises2}</p>
+            <p>{part3} {exercises3}</p>
+      </>
+    )
+}
---
+++ 1:Intro to React/courseinfo/courseinfo/src/components/Total.jsx
@@ -0,0 +1,8 @@
+import React from "react"
+
+export default function Total (props) {
+    
+    return (
+        <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+    )
+}
