41909f58f58a615f21e7af1c7a1fd64ff4d45d3c
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-05 21:44:26 -0500

    refectored to clean up Content.jsx

--- 1:Intro to React/courseinfo/courseinfo/src/components/Content.jsx
+++ 1:Intro to React/courseinfo/courseinfo/src/components/Content.jsx
@@ -4,11 +4,15 @@ import Part from "./Part"
 export default function Content (props) {
     const [part1,part2,part3] = props.parts
 
+    const partDisplay = props.parts.map(item => {
+            return (
+                <Part part={item}/>
+            )
+        })
+    
     return (
         <>
-            <Part part={part1}/>
-            <Part part={part2}/>
-            <Part part={part3}/>    
+            {partDisplay}  
         </>
     )
 }
