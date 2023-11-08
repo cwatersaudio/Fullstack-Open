d605feb65509c2e1aa5f70573e7f7d6b413e240b
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-08 15:32:41 -0500

    used bracket notation --> app works

--- 1:Intro to React/unicafe/src/components/Feedback.jsx
+++ 1:Intro to React/unicafe/src/components/Feedback.jsx
@@ -4,24 +4,29 @@ export default function Feedback(props) {
     const [feedback,updateFeedback] = React.useState({good:0,neutral:0,bad:0})
     
 
-    function butClick(event) {
+    function handleClick(event) {
         let eventName = event.target.name
-        console.log(feedback)
-        console.log(eventName)
-        let tempState = {
-            ...feedback,
-            [eventName]: 6
-        }
-        console.log("temp state is:" + tempState.eventName)
+        console.log(eventName) //here 'eventName' appears to be a string
+        console.log(feedback) //monitoring state
+        console.log(feedback.eventName) //returns 'NaN'
+        console.log(feedback.good) //returns 1 (i.e. returns value of 'good')
+        updateFeedback(prevFeedback => {
+            return{
+                ...prevFeedback,
+                [eventName]: prevFeedback[eventName] + 1 
+            }
+            
+        })
+        
         }
     
     
     return (
         <>
             <h2>Give Feedback:</h2>
-            <button name="good" onClick={butClick}>Good</button>
-            <button name="neutral" onClick={butClick}>Neutral</button>
-            <button name="bad" onClick={butClick}>Bad</button>
+            <button name="good" onClick={handleClick}>Good</button>
+            <button name="neutral" onClick={handleClick}>Neutral</button>
+            <button name="bad" onClick={handleClick}>Bad</button>
         
         </>
     )
