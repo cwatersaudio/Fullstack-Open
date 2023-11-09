0600fadddade3fa29e06ba60de6cc1cc72256b53
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-08 19:02:44 -0500

    1.7 done

--- 1:Intro to React/unicafe/src/components/Statistics.jsx
+++ 1:Intro to React/unicafe/src/components/Statistics.jsx
@@ -1,13 +1,27 @@
 import React from 'react'
 
 export default function Statistics({good,neutral,bad}) {
+    let totalVotes = good + neutral + bad
+
+    const feedbackAverage = () => {
+        return(
+            (good - bad)/totalVotes
+        )
+        }
     return (
         <>
         <h2>Statistics:</h2>
-            <p>Good: {good}</p>
-            <p>Bad: {bad}</p>
-            <p>Neutral: {neutral}</p>
-        
+           <section> 
+                <p>Good: {good}</p>
+                <p>Bad: {bad}</p>
+                <p>Neutral: {neutral}</p>
+                <p>Total Votes: {totalVotes} </p>
+            </section>
+            <section>
+                <p>Positive: {good/totalVotes}%</p>
+                <p>Average: {feedbackAverage()}</p>
+
+            </section>
         </>
     )
 }
