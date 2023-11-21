4e4665fe66abae48ae3fa6fb64455fa578cd7633
Chris Waters <48170993+cwatersaudio@users.noreply.github.com>
2023-11-21 15:22:32 -0500

    revert to 1.5

--- 1:Intro to React/courseinfo/src/App.jsx
+++ 1:Intro to React/courseinfo/src/App.jsx
@@ -1,36 +1,39 @@
-
-import React from "react"
-import Course from './components/Course'
+import Header from './components/Header.jsx'
+import Content from './components/Content.jsx'
+import Total from './components/Total.jsx'
 
 const App = () => {
   const course = {
-    id: 1,
-    name: 'Half Stack application development',
+        name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
+        exercises: 10
       },
       {
         name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
+        exercises: 7
       },
       {
         name: 'State of a component',
-        exercises: 14,
-        id: 3
+        exercises: 14
       }
     ]
   }
+
   return (
-      <>
-        
-          <Course course={course} />
-      </>
+      <div>
+      <Header course={course}/>
+      <Content
+      parts={course.parts}
+      />
+      <Total
+      parts={course.parts}
+      />
+      
+      
+    </div>
   )
-  
 }
 
 export default App
