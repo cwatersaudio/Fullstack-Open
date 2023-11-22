b6c9f9e21d9b912c07185501bd6fec726364354b
Chris Waters <chriswatersaudio@gmail.com>
2023-11-22 12:45:38 -0500

    halfway through 2.3 --> implementing .reduce()

--- 2:Communicating With Server/courseinfo/src/components/Total.jsx
+++ 2:Communicating With Server/courseinfo/src/components/Total.jsx
@@ -2,7 +2,10 @@ import React from "react"
 
 export default function Total (props) { 
         const [part1,part2,part3] = props.parts
-    return (
+        
+        const exerciseTotal = props.reduce
+    
+        return (
         <strong> Total number of exercises {part1.exercises + part2.exercises + part3.exercises}</strong>
     )
 }
